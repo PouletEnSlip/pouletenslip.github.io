3833d841378f7eea1145db580dfbf9694e40097a
PouletEnSlip <97188999+PouletEnSlip@users.noreply.github.com>
2022-03-19 16:54:18 +0100

    Create scriptOpti.js

---
+++ script/scriptOpti.js
@@ -0,0 +1,13 @@
+$("input.box").each(function() {
+    var mycookie = $.cookie($(this).attr('name'));
+    if (mycookie && mycookie == "true") {
+        $(this).prop('checked', mycookie);
+    }
+});
+$("input.box").change(function() {
+    $.cookie($(this).attr("name"), $(this).prop('checked'), {
+        path: '/',
+        expires: 365
+    });
+});
+
