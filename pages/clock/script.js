51f31fa1805534efd461fba497b42d8537017888
PouletEnSlip <97188999+PouletEnSlip@users.noreply.github.com>
2022-05-05 11:32:33 +0200

    Add files via upload

---
+++ pages/clock/script.js
@@ -0,0 +1,46 @@
+let clock = document.getElementById('clock');
+let dateActuelle = document.getElementById('date');
+
+setInterval(function() {
+    let date = new Date();
+
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+    var milli = date.getMilliseconds();
+    var ampm = "AM";
+    
+    if (h == 0) {
+        h = 12;
+    }
+    
+    if (h > 12) {
+        h = h - 12;
+        session = "PM";
+    }
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    if (milli < 10) {
+        milli = "00" + milli;
+    }
+
+    if (milli < 100) {
+        milli = "0" + milli;
+    }
+
+    clock.innerHTML = h + ":" + m + ":" + s + " " + milli + " " + ampm;
+
+    var day = date.getUTCDate();
+    var year = date.getUTCFullYear();
+
+    const jour = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    let jourActuel = jour[date.getDay()];
+
+    const mois = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    let moisActuel = mois[date.getMonth()];
+
+    dateActuelle.innerHTML = jourActuel + " " + day + " " + moisActuel + " " + year;
+})
